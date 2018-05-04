4ba316bba649494e031f776c620637a3e900e853
JoshuaKodhe <jkodhe32@gmail.com>
2018-05-05 01:41:13 +0300

    implementing the slug to allow better url display

--- subject/models.py
+++ subject/models.py
@@ -1,7 +1,18 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
 # Create your models here.
 
 
 class Subject(models.Model):
     name = models.CharField(max_length=30)
+    slug = models.SlugField(unique=True)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('subject:subject_chapters', args=[self.id, self.slug])
