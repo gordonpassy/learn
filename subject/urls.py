734bf778de0c911f77015e8b21126fc42164a78a
JoshuaKodhe <jkodhe32@gmail.com>
2018-05-05 01:43:08 +0300

    defining urls to specific subjects

--- subject/urls.py
+++ subject/urls.py
@@ -2,7 +2,10 @@ from django.conf.urls import url
 from . import views
 from django.contrib.auth.views import login
 
+app_name = 'subject'
+
 urlpatterns = [
     url(r'^$', views.index, name='homepage'),
-    url(r'^login/$', login, {'template_name': 'auth/login.html'})
+    url(r'^login/$', login, {'template_name': 'auth/login.html'}),
+    url(r'^(?P<id>\d+)(?P<subject_slug>[-\w]+)/$', views.subject_chapters, name='subject_chapters'),
 ]
