4215070c1d84421a2ac443b4c72d736eaa43d57e
JoshuaKodhe <jkodhe32@gmail.com>
2018-05-04 15:15:49 +0300

    creating view function for our homepage

--- subject/views.py
+++ subject/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    title = 'Home page'
+    args = {"title": title}
+    return render(request, "subjects/index.html", args)
