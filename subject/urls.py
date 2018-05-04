19786c5d4c71666d9428da842dbe8c125fd54b1a
JoshuaKodhe <jkodhe32@gmail.com>
2018-05-04 21:23:20 +0300

    creating a url for our login html

--- subject/urls.py
+++ subject/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from . import views
+from django.contrib.auth.views import login
 
 urlpatterns = [
     url(r'^$', views.index, name='homepage'),
+    url(r'^login/$', login, {'template_name': 'auth/login.html'})
 ]
