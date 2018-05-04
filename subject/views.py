321660450222b3a5e9e50e4e0c36ac857e35fe1a
JoshuaKodhe <jkodhe32@gmail.com>
2018-05-04 22:28:25 +0300

    adding form submition logic

--- subject/views.py
+++ subject/views.py
@@ -1,11 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import SubjectForm
+from .models import Subject
 
 # Create your views here.
 
 
 def index(request):
     title = 'Home page'
-    form = SubjectForm()
-    args = {"title": title, "subject_form": form}
+    subjects = Subject.objects.all()
+    if request.method == "POST":
+        form = SubjectForm(request.POST)
+        if form.is_valid():
+            subject = form.save(commit=False)
+            subject.author = request.user
+            subject.save()
+            return redirect('homepage')
+    else:
+        form = SubjectForm()
+    args = {"title": title, "subject_form": form, "subjects": subjects}
     return render(request, "subjects/index.html", args)
