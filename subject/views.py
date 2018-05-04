675defbf783d7009859c8a406a3b909cacc63866
JoshuaKodhe <jkodhe32@gmail.com>
2018-05-04 16:59:40 +0300

    defining the subject's form to be displayed in our html

--- subject/views.py
+++ subject/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from .forms import SubjectForm
 
 # Create your views here.
 
 
 def index(request):
     title = 'Home page'
-    args = {"title": title}
+    form = SubjectForm()
+    args = {"title": title, "subject_form": form}
     return render(request, "subjects/index.html", args)
