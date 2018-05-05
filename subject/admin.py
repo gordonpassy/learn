bb3949bdfb1e087484155d6d7175f1d1fd6c093c
JoshuaKodhe <jkodhe32@gmail.com>
2018-05-05 09:19:33 +0300

    Adding our Model Chapter to the admin panel

--- subject/admin.py
+++ subject/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Subject
+from .models import Subject, Chapter
 
 # Register your models here.
 
 admin.site.register(Subject)
+admin.site.register(Chapter)
