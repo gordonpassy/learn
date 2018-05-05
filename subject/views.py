0712df4a22b57fe98ea1afd99aaa7f3428496d3a
JoshuaKodhe <jkodhe32@gmail.com>
2018-05-05 10:53:16 +0300

    require users to login to be able to post subjects

--- subject/views.py
+++ subject/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import get_object_or_404, render, redirect
 from .forms import SubjectForm, ChapterForm
 from .models import Subject, Chapter
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 
+@login_required(login_url='/login/')
 def index(request):
     title = 'Home page'
     subjects = Subject.objects.all()
@@ -23,15 +25,18 @@ def index(request):
 
 def subject_chapters(request, id):
     subject = get_object_or_404(Subject, id=id)
+    chapters = Chapter.objects.filter(subject_id=id)
     title = subject
     if request.method == "POST":
         form = ChapterForm(request.POST)
         if form.is_valid():
             chapter = form.save()
             chapter.author = request.user
+            chapter.subject = subject
             chapter.save()
             return redirect("subject:subject_chapters", id=subject.id)
     else:
         form = ChapterForm()
-    args = {"subject": subject, "title": title, "chapter_form": form}
+    args = {"subject": subject, "title": title, "chapter_form": form, "subject_chapters": chapters}
+
     return render(request, "subjects/subject_chapters.html", args)
