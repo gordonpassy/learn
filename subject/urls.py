e00b03ca6ead56be7f81069bd21e5aa2cd524ed2
JoshuaKodhe <jkodhe32@gmail.com>
2018-05-05 13:37:28 +0300

    url for login and logout for better user experience

--- subject/urls.py
+++ subject/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
 from . import views
-from django.contrib.auth.views import login
+from django.contrib.auth.views import login, logout
 
 app_name = 'subject'
 
 urlpatterns = [
     url(r'^$', views.index, name='homepage'),
-    url(r'^login/$', login, {'template_name': 'auth/login.html'}),
+    url(r'^login/$', login, {'template_name': 'auth/login.html'}, name='login'),
+    url(r'^logout/$', logout, {'template_name': 'auth/logout.html'}, name='logout'),
     url(r'^(?P<id>\d+)/$', views.subject_chapters, name='subject_chapters'),
 ]
